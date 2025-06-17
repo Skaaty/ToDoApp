ae5a93b5663befed79c469d96463a897fb1d7ec1
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:10:50 +0200

    Add NotificationDto class definition

---
+++ TodoApi/DTOs/NotificationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.DTOs
+{
+    public class NotificationDto
+    {
+        public record NotificationDTO(int Id, int UserId, string Message, bool IsReady);
+        public record CreateNotification(int UserId, string Message);
+    }
+}
