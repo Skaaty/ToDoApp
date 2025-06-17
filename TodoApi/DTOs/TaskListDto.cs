053742b5a7221f3d4c3cb5818e26e29681a7391f
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:07:20 +0200

    Add TaskListDto class definition

---
+++ TodoApi/DTOs/TaskListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.DTOs
+{
+    public class TaskListDto
+    {
+        public record TaskListDTO(int Id, string Name, string? Description, int UserId);
+        public record CreateTaskListDTO(string Name, string? Description);
+        public record UpdateTaskListDTO(string Name, string? Description);
+    }
+}
