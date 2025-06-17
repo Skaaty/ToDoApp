3b2c4ffe247e1a4fe800d3cf00cb10f285905f7e
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-18 00:01:52 +0200

    Add UpdateTaskListValidator class definition

---
+++ TodoApi/Validation/UpdateTaskListValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using static TodoApi.DTOs.TaskListDto;
+
+namespace TodoApi.Validation
+{
+    public class UpdateTaskListValidator : AbstractValidator<UpdateTaskListDTO>
+    {
+        public UpdateTaskListValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(120);
+
+            RuleFor(y => y.Name)
+                .MaximumLength(500);
+        }
+    }
+}
