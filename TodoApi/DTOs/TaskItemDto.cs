360828438aaa58e17ba147ce86e4388e2e784ac5
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:17:46 +0200

    Add TaskItemDto class definition

---
+++ TodoApi/DTOs/TaskItemDto.cs
@@ -0,0 +1,17 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.DTOs
+{
+    public class TaskItemDto
+    {
+        public record TaskItemDTO(int Id, string Name, DateTime? DueDate,
+            PriorityLevel? Priority, bool IsCompleted, int TaskListId,
+            IEnumerable<string> Tags);
+
+        public record CreateTaskItemDTO(string Name, DateTime? DueDate,
+            PriorityLevel? Priority, int TaskListId, IEnumerable<int> TagIds);
+
+        public record UpdateTaskItemDTO(string Name, DateTime? DueDate,
+            PriorityLevel? Priority, IEnumerable<int> TagIds);
+    }
+}
