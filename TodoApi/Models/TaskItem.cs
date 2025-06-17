5e45def5a1ae379070c7deb334b81daebf9b14f2
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 22:33:15 +0200

    Add TaskItem class definition

---
+++ TodoApi/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime? DueDate { get; set; }
+        public PriorityLevel? Priority { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public int TaskListId { get; set; }
+        public TaskList? TaskList { get; set; }
+
+        public ICollection<TaskItemTag> TaskItemTags { get; set; } = [];
+    }
+}
