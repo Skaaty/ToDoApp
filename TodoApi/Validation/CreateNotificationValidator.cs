6f4376538844e534c3ff707247e20d1b66f05327
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-18 00:03:49 +0200

    Add CreateNotificationValidator class definition

---
+++ TodoApi/Validation/CreateNotificationValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using static TodoApi.DTOs.NotificationDto;
+
+namespace TodoApi.Validation
+{
+    public class CreateNotificationValidator : AbstractValidator<CreateNotificationDTO>
+    {
+        public CreateNotificationValidator()
+        {
+            RuleFor(x => x.UserId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Message)
+                .NotEmpty()
+                .MaximumLength(250);
+        }
+    }
+}
