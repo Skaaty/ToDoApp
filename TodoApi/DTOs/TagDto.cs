c00a826a3137055f22cab5a80c90e30256e92c59
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:09:04 +0200

    Add TagDto class definition

---
+++ TodoApi/DTOs/TagDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.DTOs
+{
+    public class TagDto
+    {
+        public record TagDTO(int Id, string Name);
+        public record CreateTagDTO(string Name);
+        public record UpdateTagDTO(string Name);
+    }
+}
