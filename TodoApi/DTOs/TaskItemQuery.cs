0e0cee56e511abeb320febda5085a9c621bd77a4
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:30:03 +0200

    Add TaskItemQuery class definition

---
+++ TodoApi/DTOs/TaskItemQuery.cs
@@ -0,0 +1,14 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.DTOs
+{
+    public class TaskItemQuery
+    {
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 20;
+        public bool? IsCompleted { get; init; }
+        public PriorityLevel? Priority { get; init; }
+        public DateTime? DueDate { get; init; }
+        public int? TaskId { get; init; }
+    }
+}
