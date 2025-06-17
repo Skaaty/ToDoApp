c6f1679988ab8c1fb374e4bb02c130d077dfd1f4
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 22:47:43 +0200

    Add TodoContext class definition

---
+++ TodoApi/Data/TodoContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TaskList> TaskLists => Set<TaskList>();
+        public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<Notification> Notifications => Set<Notification>();
+
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            base.OnModelCreating(b);
+
+            b.Entity<TaskItemTag>().HasKey(t => new { t.TaskItemId, t.TagId });
+            b.Entity<TaskItemTag>()
+                .HasOne(x => x.TaskItem).WithMany(x => x.TaskItemTags).HasForeignKey(x => x.TaskItemId);
+            b.Entity<TaskItemTag>()
+                .HasOne(x => x.Tag).WithMany(x => x.TaskItemTags).HasForeignKey(x => x.TagId);
+        }
+    }
+}
