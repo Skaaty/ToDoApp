e763ca311cee505d3d2c068ca947c9816124612a
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-18 00:24:30 +0200

    Add CreateTaskItemValidator, TaskItemQueryValidator, UpdateTaskItemValidator classes definitions

---
+++ TodoApi/Validation/CreateTaskItemValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using static TodoApi.DTOs.TaskItemDto;
+
+namespace TodoApi.Validation
+{
+    public class CreateTaskItemValidator : AbstractValidator<CreateTaskItemDTO>
+    {
+        public CreateTaskItemValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(120);
+
+            RuleFor(x => x.DueDate)
+                .Must(d => d is null || d.Value.Date >= DateTime.UtcNow.Date)
+                .WithMessage("Due date cannot be a date from the past.");
+
+            RuleFor(x => x.TaskListId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.TagIds)
+                .NotNull();
+
+            RuleForEach(x => x.TagIds)
+                .GreaterThan(0);
+
+            RuleFor(x => x.TagIds)
+                .Must(ids => ids.Distinct().Count() == ids.Count())
+                .WithMessage("Tag Ids must be unique.");
+        }
+    }
+}
---
+++ TodoApi/Validation/TaskItemQueryValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using TodoApi.DTOs;
+namespace TodoApi.Validation
+{
+    public class TaskItemQueryValidator : AbstractValidator<TaskItemQuery>
+    {
+        public TaskItemQueryValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PageSize)
+                .InclusiveBetween(1, 100);
+
+            RuleFor(x => x.TaskId)
+                .Must(id => id is null || id > 0);
+
+            RuleFor(x => x.DueDate)
+                .Must(d => d is null || d.Value.Date >= DateTime.UtcNow)
+                .WithMessage("DueDate cannot be a date from the past.");
+        }
+    }
+}
