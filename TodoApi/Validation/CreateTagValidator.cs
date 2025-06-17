bf98aa0fc8eed204083aabca24ff3e8c4514a2fb
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 23:57:53 +0200

    Add CreateTagValidator class definition

---
+++ TodoApi/Validation/CreateTagValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TodoApi.DTOs;
+using static TodoApi.DTOs.TagDto;
+
+namespace TodoApi.Validation
+{
+    public class CreateTagValidator : AbstractValidator<CreateTagDTO>
+    {
+        public CreateTagValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+        }
+    }
+}
