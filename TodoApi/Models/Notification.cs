16817c024e4336679b2328c55b02c256698c64a8
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-17 22:36:30 +0200

    Add Notification class definition

---
+++ TodoApi/Models/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool IsRead { get; set; }
+    }
+}
