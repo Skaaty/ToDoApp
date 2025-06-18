0a77cda74a59c78fa80df57c1e45c8470d15b023
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-18 17:59:34 +0200

    Add function that generates Jwt token and login POST method

--- TodoApi/Controllers/AuthController.cs
+++ TodoApi/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
@@ -24,6 +24,36 @@ namespace TodoApi.Controllers
             var result = await _userManager.CreateAsync(user, dto.Password);
 
             return result.Succeeded ? Ok() : BadRequest(result.Errors);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDto dto)
+        {
+            var user = await _userManager.FindByNameAsync(dto.UserName);
+            if (user is null || !await _userManager.CheckPasswordAsync(user, dto.Password))
+                return Unauthorized("Invalid Credentials");
+
+            string token = GenerateJwt(user);
+            return Ok(new { token });
+        }
+
+        private string GenerateJwt(IdentityUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName!)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_cfg["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(int.Parse(_cfg["Jwt:ExpiresHours"]!)),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
         }
 
     }
