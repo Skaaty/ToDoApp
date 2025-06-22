e5554552ff708f739cae844df8a8b78fbd4c5204
Skaaty <53066399+Skaaty@users.noreply.github.com>
2025-06-22 21:51:43 +0200

    Add CreateTaskListValidator

---
+++ TodoApi/Validation/CreateTaskListValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TodoApi.DTOs;
+
+namespace TodoApi.Validation
+{
+    public class CreateTaskListValidator : AbstractValidator<CreateTaskListDto>
+    {
+        public CreateTaskListValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(300);
+        }
+    }
+}
